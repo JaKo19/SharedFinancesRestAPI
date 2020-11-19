ae8a8c09cee9f7316b1f523886d669a8973ded6f
Jannis <jakozian7@gmail.com>
2020-11-19 22:53:46 +0100

    added EventDocument, updated Mappings in EventController for Events

--- restapi/src/main/java/com/sharedfinances/restapi/mongodb/MongoDBConfig.java
+++ restapi/src/main/java/com/sharedfinances/restapi/mongodb/MongoDBConfig.java
@@ -15,12 +15,12 @@ import java.util.List;
 @EnableMongoRepositories(basePackageClasses = EventRepository.class)
 @Configuration
 public class MongoDBConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(EventRepository personRepository) {
-        List<Person> persons = new ArrayList<>();
-        List<Debtor> debtors = new ArrayList<>();
-        debtors.add(new Debtor("Jonathan", 20, 20));
-        persons.add(new Person("Jannis", 20, 20, debtors, "j@j.com", "123"));
-        return args -> personRepository.save(new Event("Test Event", "123456", persons));
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(EventRepository personRepository) {
+//        List<Person> persons = new ArrayList<>();
+//        List<Debtor> debtors = new ArrayList<>();
+//        debtors.add(new Debtor("Jonathan", 20, 20));
+//        persons.add(new Person("Jannis", 20, 20, debtors, "j@j.com", "123"));
+//        return args -> personRepository.save(new Event("Test Event", "123456", persons));
+//    }
 }
